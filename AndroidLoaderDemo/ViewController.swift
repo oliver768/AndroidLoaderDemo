bc6e1a639050a03ed4941a65ea6b3cd5d040e0a1
Ollyver <rs9100448@gmail.com>
2020-01-08 13:33:30 +0530

    Initial Commit

---
+++ AndroidLoaderDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AndroidLoaderDemo
+//
+//  Created by Ravindra Sonkar on 08/01/20.
+//  Copyright © 2020 Ravindra Sonkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
