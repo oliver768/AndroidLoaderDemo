f62e7f3d6b05bc44e06e236a6d292d54a9220e14
Ollyver <rs9100448@gmail.com>
2020-01-09 17:53:25 +0530

    Complete Loader with text

---
+++ AndroidLoaderDemo/LoaderView.swift
@@ -0,0 +1,48 @@
+//
+//  LoaderView.swift
+//  AndroidLoaderDemo
+//
+//  Created by Ravindra Sonkar on 08/01/20.
+//  Copyright © 2020 Ravindra Sonkar. All rights reserved.
+//
+
+import UIKit
+
+class LoaderView: UIView {
+    var msgLbl = UILabel()
+    var fnLoaded = FNLoader()
+    convenience init() {
+        self.init(frame : CGRect(x: 30, y: UIScreen.main.bounds.height/2, width: (UIScreen.main.bounds.width - 60), height: 80))
+        setUpUI()
+    }
+    
+    func setUpUI() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .white
+        msgLbl.translatesAutoresizingMaskIntoConstraints = false
+        msgLbl.lineBreakMode = .byWordWrapping
+        msgLbl.numberOfLines = 0
+        msgLbl.font = UIFont(name: "", size: 15)
+        msgLbl.textAlignment = .left
+        msgLbl.center = self.center
+        fnLoaded.add()
+        fnLoaded.translatesAutoresizingMaskIntoConstraints = false
+        
+        //MARK:- FNLoader
+        self.addSubview(fnLoaded)
+        let verLoaderConst = NSLayoutConstraint(item: fnLoaded, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
+        let leadLoaderConst = NSLayoutConstraint(item: fnLoaded, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 10)
+        let heightLoaderConst = NSLayoutConstraint(item: fnLoaded, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 45)
+        let widthLoaderConst = NSLayoutConstraint(item: fnLoaded, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 45)
+
+        //MARK:- For Message Label inside Loader View
+        self.addSubview(msgLbl)
+        let topLblConst = NSLayoutConstraint(item: msgLbl, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 10)
+        let bottomLblConst = NSLayoutConstraint(item: msgLbl, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: -10)
+        let verticalLblConst = NSLayoutConstraint(item: msgLbl, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
+        let leadLblConst = NSLayoutConstraint(item: msgLbl, attribute: .leading, relatedBy: .equal, toItem: fnLoaded, attribute: .trailing, multiplier: 1, constant: 10)
+        let trailLblConst = NSLayoutConstraint(item: msgLbl, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -30)
+        self.addConstraints([verLoaderConst,leadLoaderConst,heightLoaderConst,widthLoaderConst,verticalLblConst,leadLblConst,trailLblConst,topLblConst,bottomLblConst])
+    }
+    
+}
--- AndroidLoaderDemo/ViewController.swift
+++ AndroidLoaderDemo/ViewController.swift
@@ -12,9 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnAction(_ sender: Any) {
+        addLoader("Please Wait for Response")
+    }
+    
+    func addLoader(_ msgString : String?) {
+        let loader = LoaderView()
+        loader.msgLbl.text = msgString ?? "Please Wait" 
+        loader.backgroundColor = .white
+        loader.layer.cornerRadius = 5
+        let mainView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+        mainView.backgroundColor = UIColor.clear
+        mainView.tag = 1010101
+        mainView.addSubview(loader)
+        if let appWindow = UIApplication.shared.windows.first {
+            appWindow.addSubview(mainView)
+        }
+        let horizontalConstraint = NSLayoutConstraint(item: loader, attribute: .centerX, relatedBy: .equal, toItem: mainView, attribute: .centerX, multiplier: 1, constant: 0)
+        let verticalConstraint = NSLayoutConstraint(item: loader, attribute: .centerY, relatedBy: .equal, toItem: mainView, attribute: .centerY, multiplier: 1, constant: 0)
+        let leadingConstraint = NSLayoutConstraint(item: loader, attribute: .leading, relatedBy: .equal, toItem: mainView, attribute: .leading, multiplier: 1, constant: 30)
+        let trailingConstraint = NSLayoutConstraint(item: loader, attribute: .trailing, relatedBy: .equal, toItem: mainView, attribute: .trailing, multiplier: 1, constant: -30)
+        let heightConstraint = NSLayoutConstraint(item: loader, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 70)
+        mainView.addConstraints([horizontalConstraint, verticalConstraint,leadingConstraint,trailingConstraint,heightConstraint])
+    }
+    
+    func removeLoader() {
+        if let appWindow = UIApplication.shared.windows.first {
+            appWindow.viewWithTag(1010101)?.removeFromSuperview()
+        }
+    }
 }
 
